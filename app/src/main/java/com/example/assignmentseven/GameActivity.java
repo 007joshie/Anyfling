c6d6fffb78d85f8c289734c8ce0a313556fc2beb
BOAG, JOSHUA CLAYTON (Josh) <joshua.boag@gmail.com>
2020-09-29 20:55:34 +1300

    Implemented Fullscreen mode on both the homescreen and inside the main game activity

--- app/src/main/java/com/example/assignmentseven/GameActivity.java
+++ app/src/main/java/com/example/assignmentseven/GameActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
@@ -30,6 +31,19 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        // Hide Action Bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        // Set to fullscreen
+        int uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        getWindow().getDecorView().setSystemUiVisibility(uiOptions);
+
+        String version = BuildConfig.VERSION_NAME;
 
         GraphicsView graphicsView = new GraphicsView(this);
         ConstraintLayout constraintLayout = (ConstraintLayout)findViewById(R.id.c1_game);
--- app/src/main/java/com/example/assignmentseven/MainActivity.java
+++ app/src/main/java/com/example/assignmentseven/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.assignmentseven;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -15,7 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Hello Josh
+
+        // Hide Action Bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+
+        // Set to fullscreen
+        int uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        getWindow().getDecorView().setSystemUiVisibility(uiOptions);
+
     }
 
     public void onClickStart(View v){
